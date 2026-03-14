34d5bb2e07918ebbd730c37a257e24ac32214ffb
Kalebe Felix <kalebe2006.cr@gmail.com>
2026-03-14 17:16:02 -0300

    Feat: Criando as models e migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,9 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -30,8 +30,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password'
     ];
 
     /**
@@ -42,8 +41,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'password' => 'hashed'
         ];
     }
 }
